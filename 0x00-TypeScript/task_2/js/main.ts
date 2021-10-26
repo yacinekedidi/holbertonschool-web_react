e5f89c15a2afedd6d8b9633db3c93242ef465129
yacinekedidi <kedidiyacine@gmail.com>
2021-10-26 21:16:26 +0100

    typescript

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,30 +1,16 @@
 interface DirectorInterface {
   workFromHome(): string;
-  getToWork(): string;
+  getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
 
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+  workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
-  workFromHome(): string {
-    return 'Working from home';
-  }
-
-  getToWork(): string {
-    return 'Cannot have a break';
-  }
-
-  workDirectorTasks(): string {
-    return 'Getting to director tasks';
-  }
-}
-
-class Teacher implements TeacherInterface {
   workFromHome(): string {
     return 'Working from home';
   }
@@ -38,7 +24,27 @@ class Teacher implements TeacherInterface {
   }
 }
 
-const createEmployee = (salary: number | string): Teacher | Director => {
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+interface createEmployeeFunction {
+  (salary: number | string): Teacher | Director;
+}
+
+let createEmployee: createEmployeeFunction;
+
+createEmployee = (salary: number | string): Teacher | Director => {
   if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
@@ -48,3 +54,42 @@ const createEmployee = (salary: number | string): Teacher | Director => {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+interface EmployeeFunction {
+  (employee: Director | Teacher): boolean;
+}
+
+let isDirector: EmployeeFunction;
+
+isDirector = (employee: Director | Teacher): boolean => {
+  return employee instanceof Director ? true : false;
+};
+
+interface executeWorkFunction {
+  (employee: Director | Teacher): string;
+}
+let executeWork: executeWorkFunction;
+
+executeWork = (employee: Director | Teacher): string => {
+  return isDirector(employee)
+    ? employee.workDirectorTasks()
+    : employee.workTeacherTasks();
+};
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
+
+type Subjects = 'Math' | 'History';
+
+interface teachClassFunction {
+  (todayClass: Subjects): string;
+}
+
+let teachClass: teachClassFunction;
+
+teachClass = (todayClass: Subjects): string => {
+  return `Teaching ${todayClass}`;
+};
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
