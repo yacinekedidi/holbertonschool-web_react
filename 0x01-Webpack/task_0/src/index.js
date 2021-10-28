afc6fc3a9c4df9f2130259fe3d02747658befe4c
yacinekedidi <kedidiyacine@gmail.com>
2021-10-28 21:38:49 +0100

    all

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+$ = require("jquery");
+
+$("body").append(
+	[$("<p/>"), { text: "Dashboard data for the students" }],
+	[$("<p/>"), { text: "Holberton Dashboard" }],
+	[$("<p/>"), { text: "Copyright - Holberton School" }]
+);
