711a3c014a83dbc4cc54d904989fc15d0fa7027a
yacinekedidi <kedidiyacine@gmail.com>
2021-10-29 18:55:28 +0100

    all

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 $ = require("jquery");
 
-$("body").append(
-	[$("<p/>"), { text: "Dashboard data for the students" }],
-	[$("<p/>"), { text: "Holberton Dashboard" }],
-	[$("<p/>"), { text: "Copyright - Holberton School" }]
-);
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");
