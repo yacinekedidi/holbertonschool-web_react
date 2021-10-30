0ef310274d904aee5298d76728a7b7f3a13d31e5
yacinekedidi <kedidiyacine@gmail.com>
2021-10-30 13:03:04 +0100

    1

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -2,7 +2,7 @@ const $ = require('jquery');
 const _ = require('lodash');
 
 $('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Holberton Dashboard data for the students</p>');
+$('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
