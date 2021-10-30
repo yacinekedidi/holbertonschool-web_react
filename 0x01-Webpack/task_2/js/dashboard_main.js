4d53312e229b898994313b88eed3a9f0b16a6693
yacinekedidi <kedidiyacine@gmail.com>
2021-10-30 18:20:36 +0100

    2

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import '../css/main.css';
+const $ = require('jquery');
+const _ = require('lodash');
+
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+  let count = 0;
+  const btn = $('button');
+  btn.on('click', () => {
+    count++;
+    $('p#count').text(`${count} clicks on the button`);
+  });
+}
+
+_.debounce(updateCounter);
+updateCounter();
---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
