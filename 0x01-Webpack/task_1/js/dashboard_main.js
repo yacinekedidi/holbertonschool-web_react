1b85ad66679e6d0c97010a540e59469612d0dad5
yacinekedidi <kedidiyacine@gmail.com>
2021-10-30 12:42:10 +0100

    1

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+const $ = require('jquery');
+const _ = require('lodash');
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Holberton Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+function updateCounter() {
+  const btn = $('button#count');
+  btn.on('click', () => {
+    click++;
+    btn.text(`${count} clicks on the button`);
+  });
+}
+
+_.debounce(updateCounter);
+updateCounter();
