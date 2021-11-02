04162a851a5a23a373f92d8fafb82a0df2d6e845
yacinekedidi <kedidiyacine@gmail.com>
2021-11-02 23:50:54 +0100

    2

---
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt="" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
