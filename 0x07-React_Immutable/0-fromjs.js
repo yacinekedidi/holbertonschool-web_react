9c2e39241c8e433e4e7c8682bffa97a5c628c6f9
yacinekedidi <kedidiyacine@gmail.com>
2022-06-04 12:00:55 +0100

    f

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,4 +1,6 @@
 "use strict";
 import { fromJS } from "immutable";
 
-const getImmutableObject = (object) => fromJS(object);
+export default function getImmutableObject(object) {
+  return fromJS(object);
+}
