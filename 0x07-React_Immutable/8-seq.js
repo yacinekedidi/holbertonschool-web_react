08347fcbbac7c5f1c57c2b7bb3827862d1c48305
yacinekedidi <kedidiyacine@gmail.com>
2022-06-05 11:53:50 +0100

    Seq filter

---
+++ 0x07-React_Immutable/8-seq.js
@@ -0,0 +1,18 @@
+import { Seq } from "immutable";
+
+function printBestStudents(object) {
+  const bestStudents = Seq(object)
+    .filter((student) => student.score > 70)
+    .map((student) => {
+      student.firstName = `${student.firstName[0].toUpperCase()}${student.firstName.slice(
+        1
+      )}`;
+      student.lastName = `${student.lastName[0].toUpperCase()}${student.lastName.slice(
+        1
+      )}`;
+      return student;
+    });
+  console.log(bestStudents.toJS());
+}
+
+export default printBestStudents;
