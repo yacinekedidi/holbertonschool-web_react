cc941b24d197db8d6976a580dfec1a023896fdbf
yacinekedidi <kedidiyacine@gmail.com>
2022-06-05 11:20:58 +0100

    concat and merge

---
+++ 0x07-React_Immutable/5-merge.js
@@ -0,0 +1,15 @@
+import { Map, List } from "immutable";
+
+export function concatElements(page1, page2) {
+  const p1L = List(page1);
+  const p2L = List(page2);
+  return p1L.concat(p2L);
+}
+
+console.log(concatElements([1, 2], [3, 4]));
+
+export function mergeElements(page1, page2) {
+  const p1M = Map(page1);
+  const p2M = Map(page2);
+  return List(p1M.merge(p2M));
+}
