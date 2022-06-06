69d43077cb7916ecb66fa0a4ebb74e8939f01d92
yacinekedidi <kedidiyacine@gmail.com>
2022-06-06 18:11:24 +0100

    Create actions for the UI

---
+++ 0x08_react_redux_action_creator_normalizr/task_4/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+import PropTypes from "prop-types";
+import CourseListRow from "./CourseListRow";
+import CourseShape from "./CourseShape";
+
+const CourseList = (props) => {
+  return (
+    <table id="CourseList" className={css(styles.courseList, styles.multi)}>
+      <thead className={css(styles.thead, styles.multi)}>
+        <CourseListRow isHeader={true} textFirstCell="Available courses" />
+        <CourseListRow
+          isHeader={true}
+          textFirstCell="Course name"
+          textSecondCell="Credit"
+        />
+      </thead>
+      <tbody>
+        {!props.listCourses.length ? (
+          <CourseListRow
+            isHeader={false}
+            textFirstCell="No course available yet"
+          />
+        ) : (
+          <>
+            {props.listCourses.map((course) => (
+              <CourseListRow
+                key={course.id}
+                isHeader={false}
+                textFirstCell={course.name}
+                textSecondCell={course.credit}
+              />
+            ))}
+          </>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+const styles = StyleSheet.create({
+  courseList: {
+    margin: "3rem",
+  },
+  multi: {
+    borderCollapse: "collapse",
+    border: "1px ridge",
+  },
+  thead: {
+    textAlign: "left",
+  },
+});
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
+};
+
+CourseList.defaultProps = {
+  listCourses: [],
+};
+
+export default CourseList;
---
+++ 0x08_react_redux_action_creator_normalizr/task_4/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,13 @@
+import * as uiActionTypes from "./uiActionTypes";
+
+export const login = (email, password) => ({
+  type: uiActionTypes.LOGIN,
+  user: { email, password },
+});
+export const logout = () => ({ type: uiActionTypes.LOGOUT });
+export const displayNotificationDrawer = () => ({
+  type: uiActionTypes.DISPLAY_NOTIFICATION_DRAWER,
+});
+export const hideNotificationDrawer = () => ({
+  type: uiActionTypes.HIDE_NOTIFICATION_DRAWER,
+});
