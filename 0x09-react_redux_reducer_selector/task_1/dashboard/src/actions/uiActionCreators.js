50ca4b4d682d2fe4abf5f181172f95e5b8712353
yacinekedidi <kedidiyacine@gmail.com>
2022-06-08 15:20:41 +0100

    adding immutableJs to the reducer

---
+++ 0x09-react_redux_reducer_selector/task_1/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,39 @@
+import { bindActionCreators } from "redux";
+
+import * as uiActionTypes from "./uiActionTypes";
+
+export const login = (email, password) => ({
+  type: uiActionTypes.LOGIN,
+  user: { email, password },
+});
+
+export const logout = () => ({ type: uiActionTypes.LOGOUT });
+export const displayNotificationDrawer = () => ({
+  type: uiActionTypes.DISPLAY_NOTIFICATION_DRAWER,
+});
+export const hideNotificationDrawer = () => ({
+  type: uiActionTypes.HIDE_NOTIFICATION_DRAWER,
+});
+
+export const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    { login, logout, displayNotificationDrawer, hideNotificationDrawer },
+    dispatch
+  );
+};
+
+export const loginSuccess = () => ({ type: uiActionTypes.LOGIN_SUCCESS });
+export const loginFailure = () => ({ type: uiActionTypes.LOGIN_FAILURE });
+
+export const loginRequest = (email, password) => (dispatch) => {
+  dispatch(login(email, password));
+  (async () => {
+    try {
+      const response = await fetch("http://localhost:8080/login-success.json");
+      await response.json();
+      dispatch(loginSuccess());
+    } catch (err) {
+      dispatch(loginFailure());
+    }
+  })();
+};
